562859a1ee07ad3b5b018fe5fcca3fff81fa3d97
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-13 19:58:42 +0530

    Update Colorpair_Names.py

--- Colorpair_Names.py
+++ Colorpair_Names.py
@@ -2,5 +2,5 @@ MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
 
-def get_colorpair_names(major_color, minor_color):
+def colorpair_names(major_color, minor_color):
     return f'{major_color} {minor_color}'
