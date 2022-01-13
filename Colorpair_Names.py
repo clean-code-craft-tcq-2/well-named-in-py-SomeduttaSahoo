dbc9208f97b8647bfd098ee4d2b129455e193b54
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-13 19:41:24 +0530

    Create Colorpair_Names.py

---
+++ Colorpair_Names.py
@@ -0,0 +1,6 @@
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+
+def get_colorpair_names(major_color, minor_color):
+    return f'{major_color} {minor_color}'
