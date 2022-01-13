8f498ed5aff856f12061deb9d3adf16f88454a94
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-13 19:32:33 +0530

    Update GetColor_PairNumber.py

--- GetColor_PairNumber.py
+++ GetColor_PairNumber.py
@@ -12,3 +12,9 @@ def get_color_from_pair_number(pair_number):
   if minor_index >= len(MINOR_COLORS):
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
+
+def test_number_to_pair(pair_number,
+                        expected_major_color, expected_minor_color):
+  major_color, minor_color = get_color_from_pair_number(pair_number)
+  assert(major_color == expected_major_color)
+  assert(minor_color == expected_minor_color)
