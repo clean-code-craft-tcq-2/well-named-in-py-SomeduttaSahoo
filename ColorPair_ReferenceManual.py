9afb7bf4674316aa84146721d16052d0685ca59c
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-13 21:12:28 +0530

    Update ColorPair_ReferenceManual.py

--- ColorPair_ReferenceManual.py
+++ ColorPair_ReferenceManual.py
@@ -1,7 +1,7 @@
 import Identification_colorpairs as color
 from Colorpair_Names import colorpair_names
 
-def create_reference_manual():
+def color_code_manual():
     reference_manual = []
     pair_number_start_index = 1
     pair_number_end_index = len(color.MAJOR_COLORS) * len(color.MINOR_COLORS) + 1
@@ -12,7 +12,7 @@ def create_reference_manual():
     return reference_manual
 
 
-def print_reference_manual(reference_manual):
+def print_color_code_manual(reference_manual):
     print("{:<15} {:<15}".format('Pair Number', 'Major Minor ColorPairs'))
     for manual_entries in reference_manual:
         print(manual_entries)
