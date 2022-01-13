6321ac925dd3770a70de6b6173a6a184d06b8aeb
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-13 21:14:21 +0530

    Create TestCases_colorcode.py

---
+++ TestCases_colorcode.py
@@ -0,0 +1,26 @@
+import Identification_colorpairs as color
+from ColorPair_ReferenceManual import color_code_manual
+
+def test_number_to_pair(pair_number,
+                        expected_major_color, expected_minor_color):
+  major_color, minor_color = get_color_from_pair_number(pair_number)
+  assert(major_color == expected_major_color)
+  assert(minor_color == expected_minor_color)
+
+
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+  pair_number = get_pair_number_from_color(major_color, minor_color)
+  assert(pair_number == expected_pair_number)
+  
+  def color_code_manual(expected_entries_number):
+    reference_manual = color_code_manual()
+    assert (len(reference_manual) == expected_entries_number)
+
+
+if __name__ == '__main__':
+  test_number_to_pair(4, 'White', 'Brown')
+  test_number_to_pair(5, 'White', 'Slate')
+  test_pair_to_number('Black', 'Orange', 12)
+  test_pair_to_number('Violet', 'Slate', 25)
+  test_pair_to_number('Red', 'Orange', 7)
+  print_color_code_manual()
