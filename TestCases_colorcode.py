8553c27efef3a85d01be3a520d7d9af11655bcb7
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-13 21:46:33 +0530

    Update TestCases_colorcode.py

--- TestCases_colorcode.py
+++ TestCases_colorcode.py
@@ -12,7 +12,7 @@ def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = color.get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
   
-  def color_code_manual(expected_entries_number):
+  def test_color_code_manual(expected_entries_number):
     reference_manual = color_code_manual()
     assert (len(reference_manual) == expected_entries_number)
 
@@ -23,4 +23,4 @@ def tests():
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  color_code_manual(25)
+  test_color_code_manual(25)
