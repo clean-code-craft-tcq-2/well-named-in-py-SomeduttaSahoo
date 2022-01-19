a35d20909a530e7f39fd6f44f9c2446fd61f33b2
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-01-19 11:56:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from TestCases_colorcode import tests
-from ColorPair_ReferenceManual import color_code_manual, print_color_code_manual
+from color_coder_test import execute_unittests
+from reference_manual_generator import print_reference_manual, create_reference_manual
 
 if __name__ == '__main__':
-    tests()
-    print("Reference Manualto identify color coding", )
-    reference_manual = color_code_manual()
-    print_color_code_manual(reference_manual)
+    execute_unittests()
+    print("\n Reference Manual for Telecomm Insulation Wire color codes \n", )
+    reference_manual = create_reference_manual()
+    print_reference_manual(reference_manual)
+    print('Done :)')
     
 
 
